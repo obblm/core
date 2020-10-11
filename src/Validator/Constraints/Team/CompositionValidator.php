ecde072a4a1a5f8d0e0ddd41dd8982215e4a1418
Kumulo <lanet.benjamin@gmail.com>
2020-10-11 16:34:54 +0200

    Update CompositionValidator

--- src/Validator/Constraints/Team/CompositionValidator.php
+++ src/Validator/Constraints/Team/CompositionValidator.php
@@ -3,6 +3,9 @@
 namespace Obblm\Core\Validator\Constraints\Team;
 
 use Obblm\Core\Contracts\InducementInterface;
+use Obblm\Core\Contracts\PositionInterface;
+use Obblm\Core\Contracts\RuleHelperInterface;
+use Obblm\Core\Entity\Player;
 use Obblm\Core\Entity\Team;
 use Obblm\Core\Entity\TeamVersion;
 use Obblm\Core\Exception\UnexpectedTypeException;
@@ -18,6 +21,9 @@ class CompositionValidator extends ConstraintValidator
 {
     private $ruleHelper;
     private $translator;
+    /** @var RuleHelperInterface */
+    private $helper;
+    private $count = [];
 
     public function __construct(RuleHelper $ruleHelper, TranslatorInterface $translator)
     {
@@ -39,38 +45,72 @@ class CompositionValidator extends ConstraintValidator
             throw new UnexpectedTypeException($value, TeamVersion::class);
         }
 
-        $count = [];
         /** @var TeamVersion $value */
         $team  = $value->getTeam();
-        $helper = $this->ruleHelper->getHelper($team);
+        $this->helper = $this->ruleHelper->getHelper($team);
+
+        $this->validatePlayers($value, $constraint);
+    }
+
+    private function validatePlayers($value, Composition $constraint)
+    {
         foreach ($value->getNotDeadPlayerVersions() as $version) {
             if ($version->getPlayer()->getPosition()) {
-                $element = $helper->getPlayerPosition($version->getPlayer());
-                $limit = $element->getMax();
-                $position = $element->getKey();
-                if (!$element instanceof Translatable) {
-                    throw new UnexpectedTypeException($element, Translatable::class);
-                }
-                isset($count[$position]) ? $count[$position]++ : $count[$position] = 1;
-                if ($count[$position] > $limit) {
-                    $this->context->buildViolation($constraint->limitMessage)
-                        ->setParameter('{{ limit }}', $limit)
-                        ->setParameter('{{ player_type }}', $this->translator->trans($element->getName(), [], $element->getTranslationDomain()))
-                        ->addViolation();
-                }
-                // Inducements
-                if ($element instanceof StarPlayer) {
-                    $position = $element->getTypeKey();
-                    $limit = $helper->getMaxStarPlayers();
-                    isset($count[$position]) ? $count[$position]++ : $count[$position] = 1;
-                    if ($count[$position] > $limit) {
-                        $this->context->buildViolation($constraint->limitMessage)
-                            ->setParameter('{{ limit }}', $limit)
-                            ->setParameter('{{ player_type }}', $this->translator->trans($element->getType(), [], $element->getType()->getTranslationDomain()))
-                            ->addViolation();
-                    }
-                }
+                $this->validatePlayerPosition($version->getPlayer(), $constraint);
             }
+        }
+    }
+
+    private function validatePlayerPosition(Player $player, Composition $constraint)
+    {
+        $element = $this->helper->getPlayerPosition($player);
+
+        /* To prevent bugs from constraint messages */
+        if (!$element instanceof Translatable) {
+            throw new UnexpectedTypeException($element, Translatable::class);
+        }
+
+        $this->positionCount($element, $constraint);
+
+        if ($element instanceof StarPlayer) {
+            $this->inducementsCount($element, $constraint);
+        }
+    }
+
+    /**
+     * Validate limit of positions in the team
+     * @param PositionInterface $element
+     * @param Composition $constraint
+     */
+    private function positionCount(PositionInterface $element, Composition $constraint)
+    {
+        $limit = $element->getMax();
+        $position = $element->getKey();
+
+        isset($this->count[$position]) ? $this->count[$position]++ : $this->count[$position] = 1;
+        if ($this->count[$position] > $limit) {
+            $this->context->buildViolation($constraint->limitMessage)
+                ->setParameter('{{ limit }}', $limit)
+                ->setParameter('{{ player_type }}', $this->translator->trans($element->getName(), [], $element->getTranslationDomain()))
+                ->addViolation();
+        }
+    }
+
+    /**
+     * Validate limit of star players, mercenaries, etc. in the team
+     * @param InducementInterface $element
+     * @param Composition $constraint
+     */
+    private function inducementsCount(InducementInterface $element, Composition $constraint)
+    {
+        $position = $element->getTypeKey();
+        $limit = $this->helper->getMaxStarPlayers();
+        isset($this->count[$position]) ? $this->count[$position]++ : $this->count[$position] = 1;
+        if ($this->count[$position] > $limit) {
+            $this->context->buildViolation($constraint->limitMessage)
+                ->setParameter('{{ limit }}', $limit)
+                ->setParameter('{{ player_type }}', $this->translator->trans($element->getType(), [], $element->getType()->getTranslationDomain()))
+                ->addViolation();
         }
     }
 }
