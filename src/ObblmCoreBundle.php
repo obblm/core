2f37aa115c78a9144c8aee6543ce4c30351104af
Kumulo <lanet.benjamin@gmail.com>
2022-07-14 00:41:55 +0200

    Update public path

--- src/ObblmCoreBundle.php
+++ src/ObblmCoreBundle.php
@@ -21,6 +21,7 @@ class ObblmCoreBundle extends Bundle
 
     public function build(ContainerBuilder $container): void
     {
+        $this->path = dirname(__DIR__ ) . DIRECTORY_SEPARATOR . 'src/Application';
         $container->registerExtension(new ObblmCoreInfrastructureExtension());
         $container->registerExtension(new ObblmCoreApplicationExtension());
 
